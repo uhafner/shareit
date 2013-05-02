a94bbb4de885e449e43bd7643d18e38d618de11d
Ulli Hafner <ullrich.hafner@gmail.com>
2013-05-02 13:15:57 +0200

    Reverted code: removed mockito examples and interface methods for users.

--- src/test/java/edu/hm/hafner/shareit/BenutzerVerwaltungUsecaseControllerImplTest.java
+++ src/test/java/edu/hm/hafner/shareit/BenutzerVerwaltungUsecaseControllerImplTest.java
@@ -1,17 +1,11 @@
 package edu.hm.hafner.shareit;
 
 import static org.junit.Assert.*;
-import static org.mockito.Matchers.*;
-import static org.mockito.Mockito.*;
 
-import java.util.ArrayList;
-import java.util.Collection;
 import java.util.NoSuchElementException;
 
 import org.junit.Test;
 
-import edu.hm.hafner.shareit.db.BenutzerController;
-import edu.hm.hafner.shareit.db.RegistrierungsController;
 import edu.hm.hafner.shareit.model.Benutzer;
 import edu.hm.hafner.shareit.model.Registrierung;
 import edu.hm.hafner.shareit.util.AbstractDatabaseTest;
@@ -27,6 +21,28 @@ public class BenutzerVerwaltungUsecaseControllerImplTest extends AbstractDatabas
     private static final String TEST_NACHNAME = "Hafner";
     private static final String TEST_VORNAME = "Ullrich";
 
+    /**
+     * Zeigt, dass die Neuanlage einer Registrierung funktioniert.
+     */
+    @Test
+    public void testeAnlegenEinerRegistrierung() {
+        BenutzerVerwaltungUsecaseController verwaltung = new BenutzerVerwaltungUsecaseControllerImpl();
+        pruefeErwarteteAnzahlRegistrierungen(verwaltung, 0);
+
+        verwaltung.registriereBenutzer(TEST_EMAIL, TEST_VORNAME, TEST_NACHNAME, TEST_PASSWORT);
+        pruefeErwarteteAnzahlRegistrierungen(verwaltung, 1);
+        pruefeInhaltRegistrierung(verwaltung);
+    }
+
+    private void pruefeInhaltRegistrierung(final BenutzerVerwaltungUsecaseController verwaltung) {
+        Registrierung benutzer = verwaltung.findeRegistrierung(createAdmin(), TEST_EMAIL);
+
+        assertEquals("Falscher Vorname", TEST_VORNAME, benutzer.getVorname());
+        assertEquals("Falscher Nachname", TEST_NACHNAME, benutzer.getNachname());
+        assertEquals("Falsche EMail", TEST_EMAIL, benutzer.getEmail());
+        assertEquals("Falsches Passwort", TEST_PASSWORT, benutzer.getPasswort());
+    }
+
     private Benutzer createAdmin() {
         return new Benutzer(TEST_EMAIL, TEST_VORNAME, TEST_NACHNAME, TEST_PASSWORT, true);
     }
@@ -35,165 +51,85 @@ public class BenutzerVerwaltungUsecaseControllerImplTest extends AbstractDatabas
         return new Benutzer(TEST_EMAIL, TEST_VORNAME, TEST_NACHNAME, TEST_PASSWORT, false);
     }
 
-    /**
-     * Testet, dass das Löschen der Registrierung an die Datenbank weitergeleitet wird.
-     */
-    @Test
-    public void testeLoeschenDerRegistrierung() {
-        RegistrierungsController registrierungsController = mock(RegistrierungsController.class);
-        BenutzerVerwaltungUsecaseController controller = createController(registrierungsController);
-
-        controller.loescheRegistrierung(createAdmin(), TEST_EMAIL);
-
-        verify(registrierungsController).delete(TEST_EMAIL);
+    private void pruefeErwarteteAnzahlRegistrierungen(final BenutzerVerwaltungUsecaseController verwaltung, final int expectedNumber) {
+        assertEquals("Falsche Anzahl registierte Benutzer", expectedNumber, verwaltung.findeAlleRegistrierungen(createAdmin()).size());
     }
 
-    private BenutzerVerwaltungUsecaseControllerImpl createController(final RegistrierungsController registrierungsController) {
-        return new BenutzerVerwaltungUsecaseControllerImpl(registrierungsController, mock(BenutzerController.class));
+    /**
+     * Zeigt, dass die Neuanlage einer Registrierung fehlschlägt, wenn der Benutzername bereits existiert.
+     */
+    @Test(expected = IllegalStateException.class)
+    public void testeDoppeltenBenutzernamen() {
+        BenutzerVerwaltungUsecaseController verwaltung = new BenutzerVerwaltungUsecaseControllerImpl();
+
+        verwaltung.registriereBenutzer(TEST_EMAIL, TEST_VORNAME, TEST_NACHNAME, TEST_PASSWORT);
+        String gleicheEmail = TEST_EMAIL;
+        verwaltung.registriereBenutzer(gleicheEmail, "Neuer Vorname", "Neuer Nachname", "Neues Passwort");
+    }
+
+    /**
+     * Zeigt, dass eine Exception geworfen wird, falls eine Registrierung mit einem Nutzernamen nicht existiert.
+     */
+    @Test(expected = NoSuchElementException.class)
+    public void testeEmailNichtGefunden() {
+        BenutzerVerwaltungUsecaseController verwaltung = new BenutzerVerwaltungUsecaseControllerImpl();
+
+        verwaltung.findeRegistrierung(createAdmin(), "unbekannte@email");
+    }
+
+    /**
+     * Zeigt, dass ein normaler Nutzer nicht alle vorhandenen Registrierungen auslesen kann.
+     */
+    @Test(expected = SecurityException.class)
+    public void testeAutorisierungAlleRegistrierungen() {
+        BenutzerVerwaltungUsecaseController verwaltung = new BenutzerVerwaltungUsecaseControllerImpl();
+
+        verwaltung.findeAlleRegistrierungen(createBenutzer());
+    }
+
+    /**
+     * Zeigt, dass ein normaler Nutzer nicht eine bestimmte Registrierungen auslesen kann.
+     */
+    @Test(expected = SecurityException.class)
+    public void testeAutorisierungSpezifischeRegistrierung() {
+        BenutzerVerwaltungUsecaseController verwaltung = new BenutzerVerwaltungUsecaseControllerImpl();
+
+        verwaltung.findeRegistrierung(createBenutzer(), "eine@email");
+    }
+
+    /**
+     * Zeigt, dass eine Registrierung auch wieder gelöscht werden kann.
+     */
+    @Test
+    public void testeLoeschenEinerRegistrierung() {
+        BenutzerVerwaltungUsecaseController verwaltung = new BenutzerVerwaltungUsecaseControllerImpl();
+
+        verwaltung.registriereBenutzer(TEST_EMAIL, TEST_VORNAME, TEST_NACHNAME, TEST_PASSWORT);
+        pruefeErwarteteAnzahlRegistrierungen(verwaltung, 1);
+        pruefeInhaltRegistrierung(verwaltung);
+
+        verwaltung.loescheRegistrierung(createAdmin(), TEST_EMAIL);
+        pruefeErwarteteAnzahlRegistrierungen(verwaltung, 0);
     }
 
     /**
      * Zeigt, dass ein normaler Nutzer nicht Registrierungen löschen kann.
      */
     @Test(expected = SecurityException.class)
-    public void testeBerechtigungFuerLoeschen() {
-        RegistrierungsController registrierungsController = mock(RegistrierungsController.class);
-        BenutzerVerwaltungUsecaseController controller = createController(registrierungsController);
+    public void testeAutorisierungLoeschen() {
+        BenutzerVerwaltungUsecaseController verwaltung = new BenutzerVerwaltungUsecaseControllerImpl();
 
-        controller.loescheRegistrierung(createBenutzer(), TEST_EMAIL);
+        verwaltung.loescheRegistrierung(createBenutzer(), "eine@email");
     }
 
     /**
-     * Zeigt, dass ein normaler Nutzer nicht Registrierungen sehen kann.
-     */
-    @Test(expected = SecurityException.class)
-    public void testeBerechtigungFuerSuchen() {
-        RegistrierungsController registrierungsController = mock(RegistrierungsController.class);
-        BenutzerVerwaltungUsecaseController controller = createController(registrierungsController);
-
-        controller.findeRegistrierung(createBenutzer(), TEST_EMAIL);
-    }
-
-    /**
-     * Zeigt, dass ein normaler Nutzer nicht Registrierungen sehen kann.
-     */
-    @Test(expected = SecurityException.class)
-    public void testeBerechtigungFuerAlleSuchen() {
-        RegistrierungsController registrierungsController = mock(RegistrierungsController.class);
-        BenutzerVerwaltungUsecaseController controller = createController(registrierungsController);
-
-        controller.findeAlleRegistrierungen(createBenutzer());
-    }
-
-    /**
-     * Zeigt, dass Exceptions der Datenhaltung durchgereicht werden.
-     */
-    @Test(expected = IllegalStateException.class)
-    public void testeExceptionDurchreichen() {
-        RegistrierungsController registrierungsController = mock(RegistrierungsController.class);
-        BenutzerVerwaltungUsecaseController controller = createController(registrierungsController);
-
-        doThrow(new IllegalStateException()).when(registrierungsController).delete(anyString());
-        controller.loescheRegistrierung(createAdmin(), TEST_EMAIL);
-    }
-
-    /**
-     * Testet, dass die richtige Registrierung gefunden wird.
-     */
-    @Test
-    public void testeFindenEinerDerRegistrierung() {
-        RegistrierungsController registrierungsController = mock(RegistrierungsController.class);
-        Registrierung registrierung = mock(Registrierung.class);
-        when(registrierungsController.findByPrimaryKey(TEST_EMAIL)).thenReturn(registrierung);
-        BenutzerVerwaltungUsecaseController controller = createController(registrierungsController);
-
-        Registrierung actual = controller.findeRegistrierung(createAdmin(), TEST_EMAIL);
-
-        assertSame("Nicht die gleiche Registrierung gefunden", registrierung, actual);
-    }
-
-    /**
-     * Zeigt, dass Exceptions der Datenhaltung durchgereicht werden.
+     * Zeigt, dass eine Exception geworfen wird, falls es die zu löschende Registrierung nicht gibt.
      */
     @Test(expected = NoSuchElementException.class)
-    public void testeExceptionBySucheNachPrimaryKey() {
-        RegistrierungsController registrierungsController = mock(RegistrierungsController.class);
-        BenutzerVerwaltungUsecaseController controller = createController(registrierungsController);
+    public void testeEmailBeimLoeschenNichtGefunden() {
+        BenutzerVerwaltungUsecaseController verwaltung = new BenutzerVerwaltungUsecaseControllerImpl();
 
-        doThrow(new NoSuchElementException()).when(registrierungsController).findByPrimaryKey(anyString());
-        controller.findeRegistrierung(createAdmin(), TEST_EMAIL);
-    }
-
-    /**
-     * Testet, dass alle Registrierungen gefunden werden.
-     */
-    @Test
-    public void testeFindenAllerRegistrierungen() {
-        RegistrierungsController registrierungsController = mock(RegistrierungsController.class);
-        ArrayList<Registrierung> registrierungen = new ArrayList<Registrierung>();
-        when(registrierungsController.findRegistrierungen()).thenReturn(registrierungen);
-        BenutzerVerwaltungUsecaseController controller = createController(registrierungsController);
-
-        Collection<Registrierung> actual = controller.findeAlleRegistrierungen(createAdmin());
-
-        assertSame("Nicht die gleichen Registrierungen gefunden", registrierungen, actual);
-    }
-
-    /**
-     * Überprüft, ob eine Exception geworfen wird, wenn der Benutzername schon vergeben wurde.
-     */
-    @Test(expected = IllegalStateException.class)
-    public void testeAnlegenRegistrierungNichtMoeglichDaBenutzerSchonVorhandenIst() {
-        BenutzerController benutzerController = mock(BenutzerController.class);
-        when(benutzerController.containsEmail("ist.schon@da")).thenReturn(true);
-
-        RegistrierungsController registrierungsController = mock(RegistrierungsController.class);
-        BenutzerVerwaltungUsecaseController controller = new BenutzerVerwaltungUsecaseControllerImpl(
-                registrierungsController, benutzerController);
-
-        controller.registriereBenutzer("ist.schon@da", TEST_VORNAME, TEST_NACHNAME, TEST_PASSWORT);
-    }
-
-    /**
-     * Überprüft, ob eine Exception geworfen wird, wenn die Registrierung schon vorhanden ist.
-     */
-    @Test(expected = IllegalStateException.class)
-    public void testeAnlegenRegistrierungNichtMoeglichDaRegistrierungSchonVorhandenIst() {
-        BenutzerController benutzerController = mock(BenutzerController.class);
-        when(benutzerController.containsEmail("ist.schon@da")).thenReturn(false);
-
-        RegistrierungsController registrierungsController = mock(RegistrierungsController.class);
-        when(registrierungsController.containsEmail("ist.schon@da")).thenReturn(true);
-
-        BenutzerVerwaltungUsecaseController controller = new BenutzerVerwaltungUsecaseControllerImpl(
-                registrierungsController, benutzerController);
-
-        controller.registriereBenutzer("ist.schon@da", TEST_VORNAME, TEST_NACHNAME, TEST_PASSWORT);
-    }
-
-    /**
-     * Überprüft, ob eine Exception geworfen wird, wenn der Benutzername schon vergeben wurde.
-     */
-    @Test
-    public void testeAnlegenRegistrierung() {
-        // Given
-        BenutzerController benutzerController = mock(BenutzerController.class);
-        when(benutzerController.containsEmail("ist.schon@da")).thenReturn(false);
-
-        RegistrierungsController registrierungsController = mock(RegistrierungsController.class);
-        when(registrierungsController.containsEmail("ist.schon@da")).thenReturn(false);
-
-        Registrierung registrierung = mock(Registrierung.class);
-        when(registrierungsController.create(TEST_EMAIL, TEST_VORNAME, TEST_NACHNAME, TEST_PASSWORT)).thenReturn(registrierung);
-
-        BenutzerVerwaltungUsecaseController controller = new BenutzerVerwaltungUsecaseControllerImpl(
-                registrierungsController, benutzerController);
-
-        // When
-        Registrierung actual = controller.registriereBenutzer(TEST_EMAIL, TEST_VORNAME, TEST_NACHNAME, TEST_PASSWORT);
-
-        // Then
-        assertSame("Nicht die gleiche Registrierung gefunden", registrierung, actual);
+        verwaltung.loescheRegistrierung(createAdmin(), "gibt.es@nicht");
     }
 }
 
